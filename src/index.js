90127f64816f2e7085560744341d842798a4300d
bjornoli <boe15@hi.is>
2019-11-24 18:27:24 +0000

    push

--- src/index.js
+++ src/index.js
@@ -3,11 +3,26 @@ import List from './lib/list';
 document.addEventListener('DOMContentLoaded', () => {
   const page = document.querySelector('body');
   const isLecturePage = page.classList.contains('lecture-page');
+  const btn = document.querySelector('button');
 
   if (isLecturePage) {
 
   } else {
     const list = new List();
     list.load();
+    btn.addEventListener('click', toggleBtn);
+    console.log('ha');
+
+    toggleBtn() {
+        console.log('ni');
+        if (btn.classList.contains('btn__active')) {
+            btn.classList.remove('btn__active');
+        }
+        else {
+            btn.classList.add('btn__active');
+        }
+    }
+    
+
   }
 });
